922293e76386c07d207922e25cc03d4763a618be
Alexy <lahaye.alexy1804@gmail.com>
2025-02-12 13:08:47 +0100

    TD-14 Création du module header #2

--- src/component/utilisateur/header/header.js
+++ src/component/utilisateur/header/header.js
@@ -1,17 +1,19 @@
 import React from "react";
+import {Badges_header} from "./badges_header";
 
 export function Header() {
     return(
         <div>
             <div className="uk-background-width-1-1 uk-background-muted">
+                <img className="header_bg" src="/test_malau/VIKVIK.png" />
                 <div className="grid-header">
                     <div className="item1">
-
+                        <img className="header_user_logo" src="/test_malau/champi.png" />
                     </div>
                     <div className="item2">Nom Utilisateur</div>
                     <div className="item3">Description un peu nul blablablablablabla blabla bla</div>
                     <div className="item4">
-
+                        <Badges_header/>
                     </div>
                 </div>
             </div>
