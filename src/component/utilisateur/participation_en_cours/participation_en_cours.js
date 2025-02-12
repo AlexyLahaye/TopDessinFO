4e57d81171513b5b26891de95bf94c7ccf6f1b41
Malauryyt <110236103+Malauryyt@users.noreply.github.com>
2025-02-12 11:52:13 +0100

    partie participation

--- src/component/utilisateur/participation_en_cours/participation_en_cours.js
+++ src/component/utilisateur/participation_en_cours/participation_en_cours.js
@@ -4,31 +4,52 @@ import {Card} from "./card";
 export function Participation_Conteneur(props) {
 
     // initialisation
+    const [participation, setParticipation] = useState([]);
 
     //UseEffect
+    useEffect( ()=>{
+        setParticipation(props.participation);
+
+    }, [props.participation])
 
     return (<>
 
-            <div className="travail participationConteneur uk-container-large">
-                <div className="conBackground uk-container-large uk-border-rounded">
-                    <div className=" test backgroundViolet uk-border-rounded">
+            <div className="travail  participationConteneur uk-container-large">
 
+                <div className="conBackground radius-Small uk-container-large uk-border-rounded">
+                    <div className=" partieTitre radius-Small backgroundViolet uk-border-rounded">
+                        <div className="conTitreParti uk-flex uk-align-center">
+                           <div>
+                               <h2 className="uk-padding-small uk-padding-remove"> PARTICIPATIONS EN COURS</h2>
+                           </div>
+                           <div data-uk-lightbox className="pointer">
+
+                               <div data-uk-lightbox>
+                                   <a
+                                      href="http://localhost:3001/participation"
+                                      data-type="iframe">Show all</a>
+                               </div>
+                           </div>
+
+
+                        </div>
                     </div>
                 </div>
 
-                <div className="test2 ">
+                <div className="PartieSlider ">
 
-                    <div className="noPadding uk-slider-container-offset" data-uk-slider>
+                    <div className=" uk-slider-container-offset uk-padding-remove-bottom" data-uk-slider="finite: true;">
 
                         <div className="uk-position-relative uk-visible-toggle uk-light" tabIndex="-1">
 
-                            <div className="uk-slider-items uk-child-width-1-2@s uk-grid">
+                            <div className="uk-slider-items uk-child-width-1-2@s uk-child-width-1-2@m uk-child-width-1-3@l  uk-child-width-1-4@xl uk-grid uk-padding-remove-bottom">
 
-                                {mesTachesInfo.length > 0 &&
-                                    mesTachesInfo.map((tache, cpt) => {
+                                {participation.length > 0 &&
+                                    participation.map((parti, cpt) => {
 
                                         return (
-                                            <Card />
+                                            <Card type={parti.type} etat={parti.etat} participant={parti.participant} fin={parti.date_fin} com={parti.date_com} id={parti.id}
+                                                  titre={parti.titre}/>
                                         )
                                     })
                                     }
