df1abc73f608de1ab8f8149b78aa210196d10a92
Malauryyt <110236103+Malauryyt@users.noreply.github.com>
2025-06-10 09:36:26 +0200

    ajout de la barre de nav horizontale

--- src/component/global/navbar.js
+++ src/component/global/navbar.js
@@ -18,40 +18,20 @@ export function NavbarHorizontal(props) {
 
     return (<>
 
-        <nav className="navBarHorizontale  backgroundViolet">
-            <div className="uk-container ">
-                <div data-uk-navbar>
 
-                    <div className="uk-navbar-left ">
+            <nav className="navBarHorizontale backgroundViolet" data-uk-navbar>
+                <div className="uk-navbar-left backgroundViolet">
 
-                        <a className="uk-navbar-item uk-logo" href="#" aria-label="Back to Home">Logo</a>
-
-                        <ul className="uk-navbar-nav">
-                            <li>
-                                <a href="#">
-                                    <span className="uk-icon uk-margin-xsmall-right" uk-icon="icon: star"></span>
-                                    Features
-                                </a>
-                            </li>
-                        </ul>
-
-                        <div className="uk-navbar-item">
-                            <div>Some <a href="#">Link</a></div>
-                        </div>
-
-                        <div className="uk-navbar-item">
-                            <form action="javascript:void(0)">
-                                <input className="uk-input uk-form-width-small" type="text" placeholder="Input"
-                                       aria-label="Input"/>
-                                    <button className="uk-button uk-button-default">Button</button>
-                            </form>
-                        </div>
-
-                    </div>
+                    <ul className="uk-navbar-nav">
+                        <li className="uk-active"><a href="#">Active</a></li>
+                        <li><a href="#">Item</a></li>
+                        <li><a href="#">Item</a></li>
+                        <li><a href="#">Item</a></li>
+                    </ul>
 
                 </div>
-            </div>
-        </nav>
+            </nav>
+
         </>
     );
 }
