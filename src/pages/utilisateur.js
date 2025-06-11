c727776992b2ea6734d67a8aa5cbc0ca6f644544
Alexy <lahaye.alexy1804@gmail.com>
2025-06-11 15:58:11 +0200

    Correction de la l'affichage et gestion des evenement recents
    La partie evenement recetne est prévu pour recevoir les données de l'API

--- src/pages/utilisateur.js
+++ src/pages/utilisateur.js
@@ -9,29 +9,41 @@ import {Reseau} from "../component/utilisateur/reseau_sociaux/reseau_sociaux";
 export function Utilisateur(props) {
 
     //initialisation
+
+    const [user, setUser] = useState();
+    const [aquiredBadges, setAquiredBadges] = useState([]);
     const [participation, setParticipation] = useState([]);
     const [posts, setPosts] = useState([]);
     const [ranks, setRanks] = useState([]);
+    const [recentEves, setRecentEves] = useState([]);
     const [reseaux, setReseaux] = useState([]);
 
     //useState
     useEffect( ()=>{
+        let tabUser = getInfoHeader();
+        let tabAquiredBadge = getInfoAquiredBadge()
         let tab = getInfotest();
         let tabPost = getInfoPost();
         let tabRank = getInfoRank();
+        let tabRecentEve = getInfoRecentEven();
+
         let tabReseaux = getInfoReseaux();
 
+        setUser(tabUser);
+        setAquiredBadges(tabAquiredBadge)
         setParticipation(tab);
         setPosts(tabPost);
         setRanks(tabRank);
+        setRecentEves(tabRecentEve);
         setReseaux(tabReseaux);
-
     }, [])
 
+    // Gestion de la scroll bar apparante ou non
     useEffect(() => {
         const disableScroll = () => {
             if (window.innerWidth >= 1900) {
                 document.body.style.overflow = "hidden";
+                window.scrollTo(0, 0);
             } else {
                 document.body.style.overflow = "auto";
             }
@@ -50,12 +62,17 @@ export function Utilisateur(props) {
         };
     }, []);
 
-
-
     return (<>
             <div className="canteneurUtilisateurAll uk-container-expend  uk-padding-remove-top">
+
+                {user?.id_user}
+                {posts[3]?.id}
+                {aquiredBadges[1]?.cc}
+
+
+
                 <div className="uk-container-expend">
-                    <Header/>
+                    <Header dataUser={user} aquiredBadges={aquiredBadges}/>
                 </div>
 
 
@@ -66,7 +83,7 @@ export function Utilisateur(props) {
                         <div className="reseaux uk-flex uk-flex-wrap uk-margin">
                             <Reseau reseaux={reseaux}/>
                         </div>
-                        <Evenement_recent/>
+                        <Evenement_recent recentEves={recentEves}/>
                     </div>
                     <div className="uk-width-1-2 partie_droite">
                         <div className="DernierPostContainer">
@@ -100,7 +117,7 @@ export function Utilisateur(props) {
                         <Participation_Conteneur participation={participation}/>
                     </div>
                     <div className="EvenementResponsive">
-                        <Evenement_recent/>
+                        <Evenement_recent recentEves={recentEves}/>
                     </div>
 
                     <div className=" uk-flex uk-flex-wrap uk-margin">
@@ -113,6 +130,36 @@ export function Utilisateur(props) {
         </>
     );
 }
+
+function getInfoHeader(){
+    var tab = {
+            "id_user":1,
+            //Max 13 charac
+            "nickname_user":"Zazou Studio",
+            //Max 112 charac
+            "legend_user":"Rouge, bleu, vert, jaune, toute les couleurs font vivre l'esprit",
+            "icon_user":"champi.png",
+            "bg_img_user":"spiderBG.jpg"
+        }
+
+    return tab;
+}
+
+function getInfoAquiredBadge(){
+    var tab = [
+        {"img": "/img/Badge1.png",},
+        {"img": "/img/Badge2.png",},
+        {"img": "/img/Badge5.png",},
+        {"img": "/img/Badge9.png",},
+        {"img": "/img/Badge11.png",},
+        {"img": "/img/Badge6.png",},
+        {"img": "/img/Badge10.png",},
+        {"img": "/img/Badge7.png",},
+    ]
+
+    return tab;
+}
+
 
 function getInfotest() {
 
@@ -183,8 +230,6 @@ function getInfotest() {
     }]
 
     return tab;
-
-
 }
 
 function getInfoPost() {
@@ -224,6 +269,57 @@ function getInfoRank() {
         "rang": "immortal",
         "mode": "commu"
     }]
+
+    return tab;
+}
+
+function getInfoRecentEven(){
+    var tab = [{
+        "titre":"Participation au concours du mois",
+        "date":"2024-11-11",
+        "img":null
+    },
+    {
+        "titre":"Championnat Régional",
+        "date":"2024-12-01",
+        "img":"Badge1.png"
+    },
+    {
+        "titre":"Tournoi Blitz",
+        "date":"2024-11-11",
+        "img":null
+    },
+    {
+        "titre":"Ajout post",
+        "date":"2024-12-01",
+        "img":"hwei.jpg"
+    },
+    {
+        "titre":"Tournoi Blitz",
+        "date":"2024-11-11",
+        "img":null
+    },
+    {
+        "titre":"Championnat Régional",
+        "date":"2024-12-01",
+        "img":"Badge8.png"
+    },
+    {
+        "titre":"Obtention icone 'Champimignon'",
+        "date":"2024-11-11",
+        "img":"champi.png"
+    },
+    {
+        "titre":"Championnat Régional",
+        "date":"2024-12-01",
+        "img":null
+    },
+    {
+        "titre":"1ere Place Tournoi Blitz",
+        "date":"2024-10-10",
+        "img":"Badge15.png"
+    },
+    ]
 
     return tab;
 }
