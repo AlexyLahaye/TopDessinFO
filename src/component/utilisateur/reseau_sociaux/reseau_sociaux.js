46c579958df99a5786df68903c577625abbee6fd
Malauryyt <110236103+Malauryyt@users.noreply.github.com>
2025-06-15 14:03:17 +0200

    ajout de la page parametre

--- src/component/utilisateur/reseau_sociaux/reseau_sociaux.js
+++ src/component/utilisateur/reseau_sociaux/reseau_sociaux.js
@@ -36,42 +36,48 @@ export function Reseau(props) {
                 <>
                     <div className={reseaux[0]?.instagram === null ? "uk-hidden" : "uk-padding-small"}>
                       <span className="uk-padding-small">
-                         <a href={`https://instagram.com/${reseaux[0]?.instagram}/`} className="uk-icon-button pointer" uk-icon="instagram" style={{ backgroundColor: "#201e34" }}></a>
+                         <a href={`https://instagram.com/${reseaux[0]?.instagram}/`} className="uk-icon-button pointer" uk-icon="instagram" style={{ backgroundColor: "#201e34" }}
+                            target="_blank" rel="noopener noreferrer"></a>
                           {reseaux[0]?.instagram}
                       </span>
                     </div>
 
                     <div className={reseaux[0]?.twitter === null ? "uk-hidden" : "uk-padding-small"}>
                       <span className="uk-padding-small">
-                        <a href="" className="uk-icon-button " uk-icon="twitter" style={{ backgroundColor: "#201e34" }}></a>
+                        <a href="" className="uk-icon-button " uk-icon="twitter" style={{ backgroundColor: "#201e34" }}
+                           target="_blank" rel="noopener noreferrer"></a>
                           {reseaux[0]?.twitter}
                       </span>
                     </div>
 
                     <div className={reseaux[0]?.tiktok === null ? "uk-hidden" : "uk-padding-small"}>
                       <span className="uk-padding-small">
-                        <a href="" className="uk-icon-button" uk-icon="tiktok" style={{ backgroundColor: "#201e34" }}></a>
+                        <a href="" className="uk-icon-button" uk-icon="tiktok" style={{ backgroundColor: "#201e34" }}
+                           target="_blank" rel="noopener noreferrer"></a>
                           {reseaux[0]?.tiktok}
                       </span>
                     </div>
 
                     <div className={reseaux[0]?.discord === null ? "uk-hidden" : "uk-padding-small"}>
                       <span className="uk-padding-small">
-                        <a href="" className="uk-icon-button" uk-icon="discord" style={{ backgroundColor: "#201e34" }}></a>
+                        <a href="" className="uk-icon-button" uk-icon="discord" style={{ backgroundColor: "#201e34" }}
+                           target="_blank" rel="noopener noreferrer"></a>
                           {reseaux[0]?.discord}
                       </span>
                     </div>
 
                     <div className={reseaux[0]?.twitch === null ? "uk-hidden" : "uk-padding-small"}>
                       <span className="uk-padding-small">
-                        <a href="" className="uk-icon-button" uk-icon="twitch" style={{ backgroundColor: "#201e34" }}></a>
+                        <a href="" className="uk-icon-button" uk-icon="twitch" style={{ backgroundColor: "#201e34" }}
+                           target="_blank" rel="noopener noreferrer"></a>
                           {reseaux[0]?.twitch}
                       </span>
                     </div>
 
                     <div className={reseaux[0]?.etsy === null ? "uk-hidden" : "uk-padding-small"}>
                       <span className="uk-padding-small">
-                        <a href="" className="uk-icon-button" uk-icon="etsy" style={{ backgroundColor: "#201e34" }}></a>
+                        <a href="" className="uk-icon-button" uk-icon="etsy" style={{ backgroundColor: "#201e34" }}
+                           target="_blank" rel="noopener noreferrer"></a>
                           {reseaux[0]?.etsy}
                       </span>
                     </div>
